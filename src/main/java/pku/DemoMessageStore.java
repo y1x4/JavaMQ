89f71040e92c5f569f321e911b01fef11de3bd2e
wangyixu <cauwangyixu@126.com>
2018-12-10 19:40:43 +0800

    修改bug

--- src/main/java/pku/DemoMessageStore.java
+++ src/main/java/pku/DemoMessageStore.java
@@ -3,6 +3,7 @@ package pku;
 import java.io.*;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -23,12 +24,59 @@ public class DemoMessageStore {
 
     int cnt = 0;
 
+    // 消息存储
+    HashMap<String, ArrayList<ByteMessage>> msgs = new HashMap<>();
+    // 遍历指针
+    HashMap<String, Integer> readPos = new HashMap<>();
+
+
+    // 加锁保证线程安全
+    /**
+     * @param msg
+     * @param topic
+     */
+    public synchronized void push(ByteMessage msg, String topic) {
+        if (msg == null) {
+            return;
+        }
+        if (!msgs.containsKey(topic)) {
+            msgs.put(topic, new ArrayList<>());
+        }
+        // 加入消息
+        msgs.get(topic).add(msg);
+
+    }
+
+    // 加锁保证线程安全
+    public synchronized ByteMessage pull(String queue, String topic) {
+        String k = queue + " " + topic;
+        if (!readPos.containsKey(k)) {
+            readPos.put(k, 0);
+        }
+        int pos = readPos.get(k);
+        if (!msgs.containsKey(topic)) {
+            return null;
+        }
+
+        ArrayList<ByteMessage> list = msgs.get(topic);
+        if (list.size() <= pos) {
+            return null;
+        } else {
+            ByteMessage msg = list.get(pos);
+            // 将键k的值+1，表示当前读到第pos个msg，下一次应该读+1个
+            readPos.put(k, pos + 1);
+            return msg;
+        }
+    }
+
+
+
 	// 加锁保证线程安全
 	/**
 	 * @param msg
 	 * @param topic
 	 */
-	public synchronized void push(ByteMessage msg, String topic) {
+	public synchronized void push1(ByteMessage msg, String topic) {
 		if (msg == null)
 			return;
 
@@ -80,7 +128,7 @@ public class DemoMessageStore {
 	}
 
 	// 加锁保证线程安全
-	public synchronized ByteMessage pull(String queue, String topic) {
+	public synchronized ByteMessage pull1(String queue, String topic) {
         try {
             if (! new File("./data/" + topic).exists()) // 不存在此 topic 文件
                 return null;
