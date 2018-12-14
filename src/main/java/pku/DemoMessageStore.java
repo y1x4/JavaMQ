fb57b06671f1f51f53fbc250afdf7ff62791b800
wangyixu <cauwangyixu@126.com>
2018-12-14 22:08:33 +0800

    修改bug

--- src/main/java/pku/DemoMessageStore.java
+++ src/main/java/pku/DemoMessageStore.java
@@ -108,11 +108,8 @@ public class DemoMessageStore {
                     out.write(strVal.getBytes());
                 }
             }
-*/
 
 
-            // write body's length, byte[]
-            int bodyLen = msg.getBody().length;
             if (bodyLen <= Byte.MAX_VALUE) {
                 out.writeByte(0);
                 out.writeByte(bodyLen);
@@ -123,7 +120,24 @@ public class DemoMessageStore {
                 out.writeByte(2);
                 out.writeInt(bodyLen);
             }
-            out.write(msg.getBody());
+*/
+
+
+            // write body's length, byte[]
+            int bodyLen = msg.getBody().length;
+
+            if (bodyLen >= 1024) {
+                byte[] compressedBody = compress(msg.getBody());
+                out.writeByte(1);
+                out.writeInt(compressedBody.length);
+                out.write(compressedBody);
+            } else {
+                out.writeByte(0);
+                out.writeInt(bodyLen);
+                out.write(msg.getBody());
+            }
+
+
 
 
         } catch (IOException e) {
@@ -206,11 +220,8 @@ public class DemoMessageStore {
                     headers.put(MessageHeader.getHeader(index), new String(vals));
                 }
             }
- */
 
-            // 读取 body 部分
-            byte type = in.get();
-            byte[] body;
+
             if (type == 0) {
                 body = new byte[in.get()];
             } else if (type == 1) {
@@ -218,8 +229,15 @@ public class DemoMessageStore {
             } else {
                 body = new byte[in.getInt()];
             }
-            in.get(body);
+ */
 
+            // 读取 body 部分
+            byte type = in.get();
+            byte[] body = new byte[in.getInt()];
+            in.get(body);
+            if (type == 1) {
+                body = decompress(body);
+            }
 
             // 组成消息并返回
             ByteMessage msg = new DefaultMessage(body);
